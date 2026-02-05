34719f59a1d9e59c455c5dc31b066a1693551915
mrahal17 <maya.rahal17@gmail.com>
2026-02-05 11:03:50 +0000

    Change project structure within components

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -39,3 +39,5 @@ export function Carousel() {
     </div>
   );
 }
+
+export default Carousel
---
+++ src/components/home-page/HomePage.tsx
@@ -0,0 +1,16 @@
+import Header from '../header/Header'
+import Carousel from '../carousel/Carousel';
+import SearchBar from '../search-bar/SearchBar';
+
+function HomePage() {
+
+  return (
+    <>
+      <Header />
+      <Carousel />
+      <SearchBar />
+    </>
+  )
+}
+
+export default HomePage
